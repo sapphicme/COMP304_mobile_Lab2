5f9b59f3e6d5abaff6dc20fe5e4657bd2dcf8395
Arielle Mueller <arielle_jaqueline@hotmail.com>
2021-01-25 17:28:07 -0500

    some UI work

---
+++ app/src/main/java/arielle/mueller/ariellemueller_comp304lab2/FinalActivityCash.java
@@ -0,0 +1,14 @@
+package arielle.mueller.ariellemueller_comp304lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalActivityCash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_cash);
+    }
+}
