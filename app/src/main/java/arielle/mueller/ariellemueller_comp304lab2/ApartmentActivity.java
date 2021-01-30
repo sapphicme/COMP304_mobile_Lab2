8d8dd6e9cc584604130525c6148afd761ed9b301
Arielle Mueller <arielle_jaqueline@hotmail.com>
2021-01-30 10:32:06 -0500

    added radio buttons

--- app/src/main/java/arielle/mueller/ariellemueller_comp304lab2/ApartmentActivity.java
+++ app/src/main/java/arielle/mueller/ariellemueller_comp304lab2/ApartmentActivity.java
@@ -2,7 +2,16 @@ package arielle.mueller.ariellemueller_comp304lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+import static arielle.mueller.ariellemueller_comp304lab2.R.id.apartment_one_check;
+import static arielle.mueller.ariellemueller_comp304lab2.R.id.apartment_two_check;
+import static arielle.mueller.ariellemueller_comp304lab2.R.id.radioApartmentOne;
 
 public class ApartmentActivity extends AppCompatActivity {
 
@@ -10,5 +19,14 @@ public class ApartmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartment);
+
+        final CheckBox checkBoxOne = (CheckBox) findViewById(apartment_one_check);
+        final RadioButton radioOne = (RadioButton) findViewById(radioApartmentOne);
+
+
+        if(checkBoxOne.isChecked()){
+            radioOne.setVisibility(View.VISIBLE);
+        }
     }
+
 }
